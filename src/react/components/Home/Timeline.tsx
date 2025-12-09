5ecf8f99477fe21352d8f51d4e1cfdd6f45c0c15
Surya Kencana Putra <117362201+ghubo@users.noreply.github.com>
2025-12-09 08:53:18 +0700

    Update Timeline.tsx

--- src/react/components/Home/Timeline.tsx
+++ src/react/components/Home/Timeline.tsx
@@ -45,8 +45,7 @@ const Timeline: React.FC = () => (
                     ? "bg-yellow-400"
                     : "bg-blue-600"
                 } ${
-                  new Date(event.date) < new Date() &&
-                  new Date(array[idx + 1]?.date) >= new Date()
+                  new Date(event.date) < new Date()
                     ? "animate-ping"
                     : "hidden"
                 }`}
